881478601eaf48ea507feb5cb376d1c99fdcc3b6
hashnut <ejrrb8@gmail.com>
2022-02-13 18:12:16 +0900

    .

--- ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
+++ ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeArenaBattle_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5B3D4052,
-				0x9A7C3BBB,
+				0xED04CE97,
+				0x15762F21,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
---
+++ ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/Fountain.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARENABATTLE_Fountain_generated_h
+#error "Fountain.generated.h already included, missing '#pragma once' in Fountain.h"
+#endif
+#define ARENABATTLE_Fountain_generated_h
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_SPARSE_DATA
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_RPC_WRAPPERS
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFountain(); \
+	friend struct Z_Construct_UClass_AFountain_Statics; \
+public: \
+	DECLARE_CLASS(AFountain, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), NO_API) \
+	DECLARE_SERIALIZER(AFountain)
+
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAFountain(); \
+	friend struct Z_Construct_UClass_AFountain_Statics; \
+public: \
+	DECLARE_CLASS(AFountain, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), NO_API) \
+	DECLARE_SERIALIZER(AFountain)
+
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFountain(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFountain) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFountain); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFountain); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFountain(AFountain&&); \
+	NO_API AFountain(const AFountain&); \
+public:
+
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFountain(AFountain&&); \
+	NO_API AFountain(const AFountain&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFountain); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFountain); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AFountain)
+
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_PRIVATE_PROPERTY_OFFSET
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_9_PROLOG
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_SPARSE_DATA \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_RPC_WRAPPERS \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_INCLASS \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ArenaBattle_Source_ArenaBattle_Fountain_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_SPARSE_DATA \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_INCLASS_NO_PURE_DECLS \
+	ArenaBattle_Source_ArenaBattle_Fountain_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE_API UClass* StaticClass<class AFountain>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ArenaBattle_Source_ArenaBattle_Fountain_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
