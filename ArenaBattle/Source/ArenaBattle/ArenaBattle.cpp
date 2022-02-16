4225172f5530c3350b03e73d92224fc53ed614ec
hashnut <ejrrb8@gmail.com>
2022-02-16 22:06:34 +0900

    .

--- ArenaBattle/Source/ArenaBattle/ArenaBattle.cpp
+++ ArenaBattle/Source/ArenaBattle/ArenaBattle.cpp
@@ -1,7 +1,9 @@
+
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ArenaBattle.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(ArenaBattle);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
  
